65cadda58818a998cca453584a22ef5c2770257e
Gilles30 <lorison.gilles@gmail.com>
2022-05-05 21:43:48 -0300

    feat: created feedback button

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#f97317',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
